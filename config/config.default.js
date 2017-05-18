8f098a6416da4d69631eb07c3637082cca25df2e
songzhengxu <2944434952@qq.com>
2017-05-18 18:02:31 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1495101350467_9495';
+
+  // add your config here
+
+  return config;
+};
